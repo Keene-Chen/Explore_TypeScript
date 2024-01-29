049e119160c4baf9259224f6573288aa71b6ac2b
KeeneChen <keenechen@qq.com>
2024-01-29 12:56:27 +0800

    refactor: 更新路由配置

--- 02_Exercise/demo-ts-axios/src/router/index.ts
+++ 02_Exercise/demo-ts-axios/src/router/index.ts
@@ -4,12 +4,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/HomeView.vue')
+    component: () => import('@/views/home/HomeView.vue')
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('@/views/AboutView.vue')
+    component: () => import('@/views/home/AboutView.vue')
   },
   {
     path: '/axios',
